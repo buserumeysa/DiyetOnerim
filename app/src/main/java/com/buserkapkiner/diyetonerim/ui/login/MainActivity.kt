c36bbff5ca5a13a22ff389b52c913778872bcb18
Buse Rümeysa Kapkıner <buserumeysa19fb@gmail.com>
2023-02-05 23:51:48 +0300

    Firebase de login işlemi

--- app/src/main/java/com/buserkapkiner/diyetonerim/ui/login/MainActivity.kt
+++ app/src/main/java/com/buserkapkiner/diyetonerim/ui/login/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.buserkapkiner.diyetonerim.databinding.ActivityMainBinding
 import com.buserkapkiner.diyetonerim.ui.bmi.BodyMossIndexActivity
 import com.buserkapkiner.diyetonerim.ui.register.RegisterActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    val auth = Firebase.auth
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -16,12 +19,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-            intent = Intent(applicationContext, BodyMossIndexActivity::class.java)
-            startActivity(intent)
+            var userName = binding.edtTxtUserName.text.toString()
+            var userPassword = binding.edtTxtPassword.text.toString()
+            login(email = userName, password = userPassword)
         }
         binding.btnRegister.setOnClickListener {
             intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    intent = Intent(applicationContext, BodyMossIndexActivity::class.java)
+                    startActivity(intent)
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                }
+            }
     }
 }
