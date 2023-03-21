1d6da4dc8c26cf234255caba9b1f69574c5f9dcb
buserumeysa <buserumeysakapkiner@gmail.com>
2023-03-22 00:13:11 +0300

    navigation eklendi

--- app/src/main/java/com/buserkapkiner/diyetonerim/ui/login/MainActivity.kt
+++ app/src/main/java/com/buserkapkiner/diyetonerim/ui/login/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.buserkapkiner.diyetonerim.ui.login
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -8,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.buserkapkiner.diyetonerim.R
 import com.buserkapkiner.diyetonerim.databinding.ActivityMainBinding
 import com.buserkapkiner.diyetonerim.ui.bmi.BodyMossIndexActivity
-import com.buserkapkiner.diyetonerim.ui.homepage.HomePageActivity
 import com.buserkapkiner.diyetonerim.ui.register.RegisterActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val userPassword = findViewById<EditText>(R.id.edt_txt_password)
         userPassword.setAutofillHints(View.AUTOFILL_HINT_PASSWORD)
-        val userName= findViewById<EditText>(R.id.edt_txt_user_name)
+        val userName = findViewById<EditText>(R.id.edt_txt_user_name)
         userName.setAutofillHints(View.AUTOFILL_HINT_USERNAME)
 
         super.onCreate(savedInstanceState)
@@ -33,19 +33,14 @@ class MainActivity : AppCompatActivity() {
             var userName = binding.edtTxtUserName.text.toString()
             var userPassword = binding.edtTxtPassword.text.toString()
             login(email = userName, password = userPassword)
-            intent = Intent(applicationContext, HomePageActivity::class.java)
-            startActivity(intent)
+
         }
         binding.btnRegister.setOnClickListener {
             intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent)
         }
 
-
-
-
     }
-
 
 
     fun login(email: String, password: String) {
@@ -59,7 +54,8 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     // If sign in fails, display a message to the user.
                     Toast.makeText(
-                        baseContext, "Giriş Yapılamadı, Lütfen e-mail adresini doğru girdiğinizden emin olun.",
+                        baseContext,
+                        "Giriş Yapılamadı, Lütfen e-mail adresini doğru girdiğinizden emin olun.",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
