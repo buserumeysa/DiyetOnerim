180f8e51fdd888605e2cf38328152c4f1479410f
buserumeysa <buserumeysakapkiner@gmail.com>
2023-03-27 01:11:54 +0300

    Güncelleme

--- app/src/main/java/com/buserkapkiner/diyetonerim/ui/calorieCounter/CalorieCounterActivity.kt
+++ app/src/main/java/com/buserkapkiner/diyetonerim/ui/calorieCounter/CalorieCounterActivity.kt
@@ -1,6 +1,13 @@
 package com.buserkapkiner.diyetonerim.ui.calorieCounter
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.buserkapkiner.diyetonerim.R
 
 class CalorieCounterActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calorie_counter)
+    }
 }
